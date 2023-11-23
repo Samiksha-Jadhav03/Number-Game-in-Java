9446a18c613b8a232a5c7f90d9ebff4afdfcc115
Samiksha-Jadhav03 <151814098+Samiksha-Jadhav03@users.noreply.github.com>
2023-11-23 13:11:39 +0530

    Update Number Game.java

--- Number Game.java
+++ Number Game.java
@@ -1,32 +1,51 @@
+// task_01 NUMBER GAME //
 import java.util.Random;
 import java.util.Scanner;
 
-public class RandomNumberGuessingGame {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100) + 1; // Generate a random number between 1 and 100
-        int attempts = 0;
-        boolean hasWon = false;
+class GuessingGame {
+    private int numberToGuess;
+    private int attempts;
+    private int maxAttempts;
 
+    public GuessingGame(int maxAttempts) {
+        Random random = new Random();
+        numberToGuess = random.nextInt(100) + 1;
+        attempts = 0;
+        this.maxAttempts = maxAttempts;
+    }
+
+    public void play() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Random Number Guessing Game!");
+        System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
-        
-        while (!hasWon) {
-            System.out.print("Take a guess: ");
+
+        while (attempts < maxAttempts) {
+            System.out.print("Take guess no " + (attempts + 1) + ": "); 
             int guess = scanner.nextInt();
             attempts++;
 
-            if (guess == randomNumber) {
-                hasWon = true;
-            } else if (guess < randomNumber) {
-                System.out.println("Too low! Try again.");
+            if (guess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high!");
             } else {
-                System.out.println("Too high! Try again.");
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                scanner.close();
+                return;
             }
         }
 
-        System.out.println("Congratulations! You guessed the number " + randomNumber + " in " + attempts + " attempts.");
+        System.out.println("Sorry, you've reached the maximum number of attempts.");
+        System.out.println("The number I was thinking of was: " + numberToGuess);
+        scanner.close();
+    }
+}
+
+public class Task_1_NUmber_Game {
+    public static void main(String[] args) {
+        int maxAttempts = 5; // attempts limit 
+        GuessingGame guessingGame = new GuessingGame(maxAttempts);
+        guessingGame.play();
     }
 }
